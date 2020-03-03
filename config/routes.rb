766711761f44c93be8dea937f189577d8c4c9602
Philippe Fonzin <pfonzin@gmail.com>
2020-03-03 16:33:15 -0500

    Deleted new and destroy from users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#root' 
+  root 'static_pages#root' 
   
   namespace :api, defaults: { format: :json } do 
-    resources :users, only: [:new, :create, :destroy]
+    resources :users, only: [:create]
     resource :session, only: [:new, :create, :destroy]
   end 
 end
