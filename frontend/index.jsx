940f4c5a140bcd84c66d53f314de5b54bbea1896
Philippe Fonzin <pfonzin@gmail.com>
2020-03-03 00:44:31 -0500

    Added a receiver to getElementById

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-DOM';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = getElementById('root'); 
+  const root = document.getElementById('root'); 
   ReactDOM.render(<h1>Hello Young World</h1>, root);
 });
