331c1d18dfa52db8db49c7107a426af6d861e9e3
Philippe Fonzin <pfonzin@gmail.com>
2020-03-03 12:17:23 -0500

    Created thunk action creators

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -6,3 +6,12 @@ import {
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER"; 
+
+const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user, 
+})
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+})
