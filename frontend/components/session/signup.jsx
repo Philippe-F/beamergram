d52980a48466d5947d4ba30a062564ff54ed76ee
Philippe Fonzin <pfonzin@gmail.com>
2020-03-03 13:13:20 -0500

    Added a constructor and state prop to signup component

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+    }
+  }
+
   render() {
     return (
     <div></div>
