adc54a7b5f71fc3a72526f60c96d36ef90cf1968
Philippe Fonzin <pfonzin@gmail.com>
2020-03-03 16:00:53 -0500

    imported entities reducer to root reducer

--- frontend/reducers/entities.js
+++ frontend/reducers/entities.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux';
-import usersReducer from './users'; 
+import { combineReducers } from "redux";
+import usersReducer from "./users"; 
 
 const entitiesReducer = combineReducers({
   users: usersReducer
