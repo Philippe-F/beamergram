e07cd5cf9612bf57d66d828070af4b70687c40fd
Philippe Fonzin <pfonzin@gmail.com>
2020-03-03 00:04:34 -0500

    Removed an extra ] from the end of session route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do 
     resources :users, only: [:new, :create, :destroy]
-    resource :session, only: [:new, :create, :destroy]]
+    resource :session, only: [:new, :create, :destroy]
   end 
 end
