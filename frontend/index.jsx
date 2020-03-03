e57ae24fd4b88ab6a77d8e4ea3c0905e60266957
Philippe Fonzin <pfonzin@gmail.com>
2020-03-03 00:21:14 -0500

    Added an EventListener to index.jsx

---
+++ frontend/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-DOM';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = getElementById('root'); 
+  ReactDOM.render(<h1>Hello Young World</h1>, root);
+});
