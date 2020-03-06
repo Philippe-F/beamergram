2556fb0300f8bc84059b05c4d89071bc1fa567b9
Philippe Fonzin <pfonzin@gmail.com>
2020-03-06 18:09:53 -0500

    Created post routes and posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,15 +1,44 @@
 class PostsController < ApplicationController
 
-  def create
+  def index 
+    @posts = Post.all 
+    render "/api/posts"
+  end 
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render "api/posts/show"
+    else 
+      render json: @post.errors.full_messages, status: 422
+    end 
   end 
 
   def show
+    @post = current_user.posts.find_by(id: params[:id]) 
 
+    if @post 
+      render "/api/{current_user.id}/posts/@post.id"
+    else 
+      render json: @post.errors.full_messages, status: 422
+    end 
+  end 
+
+  def update
+    @post = current_user.posts.find(id: params[:id]) 
+    @post.update_attributes(post_params) 
+
+    if @post.save
+      render "/api/posts/@post.id"
+    else 
+      render json: @post.errors.full_messages, status: 422
+    end 
   end 
 
   def destroy 
-
+    @post = current_user.posts.find(id: params[:id]) 
+    @post.destroy
   end 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'static_pages#root' 
   
   namespace :api, defaults: { format: :json } do 
-    resources :users, only: [:create]
+    resources :users, only: [:create] do 
+      resources :posts, only: [:index, :create, :show, :update, :destroy]
+    end 
+
+    resources :posts, only: [:index, :show]
     resource :session, only: [:new, :create, :destroy]
   end 
 end
