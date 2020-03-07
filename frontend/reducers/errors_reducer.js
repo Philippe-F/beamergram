5f438c1383bf9e14adefe2ff024ae719cef983d8
Philippe Fonzin <pfonzin@gmail.com>
2020-03-07 14:52:02 -0500

    Created posts container

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import sessionErrorsReducer from "./session_errors_reducer";
+import posts_errors_reducer from "./posts_errors_reducer";
 
 export default combineReducers({
   session: sessionErrorsReducer,
+  posts: posts_errors_reducer,
 });
