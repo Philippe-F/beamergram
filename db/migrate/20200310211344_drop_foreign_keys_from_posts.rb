700cdb05401561b7a3adb5f8dbbdb7dc30284c22
Philippe Fonzin <pfonzin@gmail.com>
2020-03-10 17:19:26 -0400

    Ran migration to remove like_id and comment_id from posts

--- db/migrate/20200310211344_drop_foreign_keys_from_posts.rb
+++ db/migrate/20200310211344_drop_foreign_keys_from_posts.rb
@@ -1,4 +1,6 @@
 class DropForeignKeysFromPosts < ActiveRecord::Migration[5.2]
   def change
+    remove_column :posts, :like_id
+    remove_column :posts, :comment_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_203830) do
+ActiveRecord::Schema.define(version: 2020_03_10_211344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,8 +41,6 @@ ActiveRecord::Schema.define(version: 2020_03_10_203830) do
     t.datetime "updated_at", null: false
     t.text "caption", null: false
     t.integer "user_id", null: false
-    t.integer "like_id", null: false
-    t.integer "comment_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
