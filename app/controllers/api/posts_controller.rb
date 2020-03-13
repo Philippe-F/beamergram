1407e194a6b51a6082b4567a1055b43e05269030
Philippe Fonzin <pfonzin@gmail.com>
2020-03-13 15:54:45 -0400

    Change create post controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -6,8 +6,7 @@ class Api::PostsController < ApplicationController
   end 
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id 
+    @post = current_user.posts.new(post_params)
     
     if @post.save
       render :show
