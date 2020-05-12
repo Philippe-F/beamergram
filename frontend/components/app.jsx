07cb2594383986b65a04039963970e7c03b4d998
Philippe Fonzin <pfonzin@gmail.com>
2020-05-12 16:22:40 -0400

    Add user profile route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,11 +3,11 @@ import { AuthRoute, ProtectedRoute } from "../utils/route_util";
 import { Switch, Redirect } from "react-router-dom";
 import SignUpContainer from "./session/signup_container"; 
 import LogInContainer from "./session/login_container";
-import GreetingContainer from "./greeting/greeting_container";
 import PostIndexContainer from "./posts/post_index_container";
 import PostShowContainer from "./posts/post_show_container";
 import CreatePostFormContainer from "./posts/create_post_form_container";
 import EditPostFormContainer from "./posts/edit_post_form_container";
+import UserShowContainer from "./profile/user_show_container";
 
 
 const App = () => (
@@ -20,6 +20,7 @@ const App = () => (
       <ProtectedRoute path="/posts/:postId/show" component={PostShowContainer} />  
       <ProtectedRoute path="/posts/new" component={CreatePostFormContainer} />  
       <ProtectedRoute path="/posts/:postId/edit" component={EditPostFormContainer} /> 
+      <ProtectedRoute path="/users/:userId" component={UserShowContainer} />
       <Redirect to="/posts/explore" /> 
     </Switch>
   </div>
