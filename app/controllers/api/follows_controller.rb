84cc71ac8334b7a525f52f954059ec2209566071
Philippe Fonzin <pfonzin@gmail.com>
2020-05-14 13:19:06 -0400

    Create Follows controller

---
+++ app/controllers/api/follows_controller.rb
@@ -0,0 +1,30 @@
+class Api::FollowsController < ApplicationController 
+
+  def create 
+    @follow = Follow.new(follow_params)
+    @follow.user_id = current_user.id 
+
+    if @follow.save
+      render :show 
+    else
+      render json: @follow.errors.full_messages, status: 422
+    end
+  end
+
+   def show 
+    @follow = Follow.find_by(id: params[:id])
+   end
+
+  def destroy 
+    @follow = Follow.find_by!(user_id:current_user.id, followed_user_id:params[:id])
+
+    @follow.destroy
+    render :show
+  end
+
+
+  private 
+  def follow_params
+    params.require(:follow).permit(:user_id,:followed_user_id)
+  end
+end
