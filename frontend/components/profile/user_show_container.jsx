a9a75c02b6fc482d80b881e29ab9e40f2a534287
Philippe Fonzin <pfonzin@gmail.com>
2020-05-14 17:06:02 -0400

    Add follows to user show container

--- frontend/components/profile/user_show_container.jsx
+++ frontend/components/profile/user_show_container.jsx
@@ -9,12 +9,26 @@ const mapStateToProps = (state, ownProps) => {
   const userProfile = state.entities.users[profileId];
 
   let userPosts = null;
+  let followBool = false;
+  let followerIds = null;
   let currentUser = state.entities.users[state.session.id];
+
+
+  if (userProfile) {
+    userPosts = Object.values(state.entities.posts).filter(
+      post => post.user_id === userProfile.id
+    );
+
+    (followerIds = userProfile.followerIds),
+    (followBool = followerIds.includes(currentUser.id));
+  }
 
   return {
     currentUser,
     userPosts,
     userProfile,
+    followerIds,
+    followBool,
   };
 }
 
@@ -22,6 +36,8 @@ const mapDispatchToProps = (dispatch) => ({
   fetchUser: userId => dispatch(fetchUser(userId)),
   deleteUser: userId => dispatch(deleteUser(userId)),
   logout: () => dispatch(logout()),
+  createFollow: follow => dispatch(createFollow(follow)),
+  deleteFollow: follow => dispatch(deleteFollow(follow)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserShow);
