37f2f7a89d09d9f57c77403475748ac360994075
Philippe Fonzin <pfonzin@gmail.com>
2020-05-14 13:03:16 -0400

    Follow associations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,11 @@
 class Follow < ApplicationRecord
+  validates :user_id, uniqueness: {scope: :followed_user_id}
 
+  belongs_to :follower, 
+  foreign_key: :user_id,
+  class_name: :User
 
+  belongs_to :followed,
+  foreign_key: :followed_user_id,
+  class_name: :User
 end
