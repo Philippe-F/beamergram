0c3f7381b698ed48d3f567a73d9944892b25fcfd
Philippe Fonzin <pfonzin@gmail.com>
2020-05-18 12:37:33 -0400

    Create comments reducer

---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,24 @@
+import {
+  RECEIVE_POST_COMMENTS,
+  RECEIVE_COMMENT,
+  REMOVE_COMMENT
+} from "../actions/comment_actions";
+
+const commentsReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  let newState;
+
+  switch (action.type) {
+    case RECEIVE_POST_COMMENTS:
+      return action.comments;
+    case RECEIVE_COMMENT:
+      return newState[action.comment.comment.id] = action.comment.comment;
+    case REMOVE_COMMENT:
+      delete newState[action.comment.comment.id];
+      return newState;
+    default:
+      return state;
+  }
+};
+export default commentsReducer;
