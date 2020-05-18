4adf1cf3d360a14a86e0c77662927dc22045d915
Philippe Fonzin <pfonzin@gmail.com>
2020-05-18 10:38:55 -0400

    Comment controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,36 @@
+class Api::CommentsController < ApplicationController
+
+  def index 
+    @comments = Comment.all.where(post_id: params[:post_id])
+  end
+
+  def create 
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id 
+
+    if @comment.save
+      render :show
+    else
+      render json: ["Comment cannot be blank"], status: 422
+    end
+  end
+
+  def show 
+    @comment = Comment.find_by(id: params[:id])
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    if @comment && (@comment.user_id == current_user.id)
+      @comment.destroy
+      render :show
+    else
+      render json: ["User can only remove their own comments"], status: 422
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:post_id, :body)
+  end
+end
